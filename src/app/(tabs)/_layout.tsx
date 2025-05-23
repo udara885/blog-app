ba1d993617585b0f2c18610624f1de4ca3f93ddf
Udara Lakshan <ulakshan061@gmail.com>
2025-05-23 11:38:39 +0530

    Enhance TabsLayout to support dark mode styling for header, tab bar, and settings modal

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -4,7 +4,7 @@ import { Tabs } from 'expo-router';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import Feather from '@expo/vector-icons/Feather';
 import AntDesign from '@expo/vector-icons/AntDesign';
-import { Modal, Pressable, Text, View } from 'react-native';
+import { Modal, Pressable, Text, useColorScheme, View } from 'react-native';
 import { useState } from 'react';
 
 const arr1 = ['Dark mode', 'Notification', 'Clear cache', 'Change font size'];
@@ -25,17 +25,26 @@ const TabsLayout = () => {
     setIsBottomSheetVisible(false);
   };
 
+  const theme = useColorScheme();
+
   return (
     <>
-      <Tabs>
+      <Tabs
+        screenOptions={{
+          tabBarStyle: {
+            backgroundColor: theme === 'dark' ? '#212529' : 'white',
+          },
+        }}>
         <Tabs.Screen
           name="index"
           options={{
+            headerStyle: { backgroundColor: theme === 'dark' ? 'black' : 'white' },
+            headerTintColor: theme === 'dark' ? 'white' : 'black',
             headerTitle: 'I TIPS',
             title: 'Main page',
             headerRight: () => (
               <Pressable className="mr-4" onPress={openBottomSheet}>
-                <Feather name="settings" size={24} color="black" />
+                <Feather name="settings" size={24} color={theme === 'dark' ? 'white' : 'black'} />
               </Pressable>
             ),
             tabBarIcon: (tabInfo) => {
@@ -85,17 +94,17 @@ const TabsLayout = () => {
       <Modal visible={isBottomSheetVisible} transparent={true} onRequestClose={closeBottomSheet}>
         <View className="flex-1 justify-end">
           <Pressable onPress={closeBottomSheet} className="absolute inset-0 bg-black/50" />
-          <View className="rounded-t-2xl bg-gray-100 p-5">
+          <View className="rounded-t-2xl bg-gray-100 p-5 dark:bg-[#212529]">
             <View className="mb-5 flex flex-row items-center justify-between">
-              <Text className="text-3xl font-bold">Settings</Text>
+              <Text className="text-3xl font-bold dark:text-white">Settings</Text>
               <AntDesign name="closecircle" size={20} color="gray" onPress={closeBottomSheet} />
             </View>
-            <View className="rounded-xl bg-white">
+            <View className="rounded-xl bg-white dark:bg-[#343A40]">
               {arr1.map((item, index) => (
                 <View
                   key={index}
-                  className="mx-3 flex flex-row items-center justify-between border-b-2 border-gray-100 py-3">
-                  <Text className="text-lg font-bold">{item}</Text>
+                  className={`${index !== arr1.length - 1 && 'border-b-2 border-gray-100 dark:border-gray-500'} mx-3 flex flex-row items-center justify-between py-3`}>
+                  <Text className="text-lg font-bold dark:text-white">{item}</Text>
                   <View className="flex flex-row items-center gap-1">
                     {index === 0 && <Text className="text-lg text-gray-500">System</Text>}
                     {index !== 2 && <AntDesign name="right" size={20} color="lightgray" />}
@@ -106,14 +115,18 @@ const TabsLayout = () => {
             <Text className="mt-2 pl-3 text-sm text-gray-500">
               Changing the font size will apply to the entire application.
             </Text>
-            <View className="mt-2 rounded-xl bg-white">
+            <View className="mt-2 rounded-xl bg-white dark:bg-[#343A40]">
               {arr2.map((item, index) => (
                 <View
                   key={index}
-                  className="mx-3 flex flex-row items-center justify-between border-b-2 border-gray-100 py-3">
-                  <Text className="text-lg font-bold">{item.title}</Text>
+                  className={`mx-3 flex flex-row items-center justify-between ${index !== arr2.length - 1 && 'border-b-2 border-gray-100 dark:border-gray-500'} py-3`}>
+                  <Text className="text-lg font-bold dark:text-white">{item.title}</Text>
                   <View className="flex flex-row items-center gap-1">
-                    <FontAwesome name={item.icon} size={24} color="black" />
+                    <FontAwesome
+                      name={item.icon}
+                      size={24}
+                      color={theme === 'dark' ? 'white' : 'black'}
+                    />
                   </View>
                 </View>
               ))}
