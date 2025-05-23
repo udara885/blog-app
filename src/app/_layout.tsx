20b9525557467ea377659868f7b301a334538be6
Udara Lakshan <ulakshan061@gmail.com>
2025-05-23 11:38:29 +0530

    Enhance RootLayout to manage StatusBar appearance based on navigation and theme

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,8 +1,27 @@
 import Feather from '@expo/vector-icons/Feather';
-import { Stack } from 'expo-router';
-import { Pressable, View } from 'react-native';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { Stack, usePathname } from 'expo-router';
+import { useEffect } from 'react';
+import { Pressable, StatusBar, useColorScheme, View } from 'react-native';
 
 const RootLayout = () => {
+  const pathname = usePathname();
+
+  const theme = useColorScheme();
+
+  useEffect(() => {
+    if (pathname === '/ArticleScreen') {
+      StatusBar.setBarStyle('light-content');
+      StatusBar.setBackgroundColor('black');
+    } else if (theme === 'dark') {
+      StatusBar.setBarStyle('light-content');
+      StatusBar.setBackgroundColor('#FFFFFF');
+    } else {
+      StatusBar.setBarStyle('dark-content');
+      StatusBar.setBackgroundColor('#FFFFFF');
+    }
+  }, [pathname, theme]);
+
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
@@ -11,13 +30,15 @@ const RootLayout = () => {
         options={{
           headerTitle: '',
           headerShadowVisible: false,
+          headerTransparent: true,
+          headerTintColor: 'white',
           headerRight: () => (
             <View className="flex flex-row items-center">
               <Pressable className="mr-4">
-                <Feather name="bookmark" size={24} color="black" />
+                <FontAwesome name="bookmark-o" size={24} color="white" />
               </Pressable>
               <Pressable className="mr-4">
-                <Feather name="share" size={24} color="black" />
+                <Feather name="share" size={24} color="white" />
               </Pressable>
             </View>
           ),
