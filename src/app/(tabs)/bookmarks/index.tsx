e1fdb82cc1a2dbe4c3e4e9918df804e9ed724caa
Udara Lakshan <ulakshan061@gmail.com>
2025-05-23 11:39:46 +0530

    Enhance Bookmarks screen to display bookmarked articles with navigation to ArticleScreen

--- src/app/(tabs)/bookmarks/index.tsx
+++ src/app/(tabs)/bookmarks/index.tsx
@@ -1,14 +1,22 @@
+import { Link } from 'expo-router';
 import { View, Text, ScrollView } from 'react-native';
 import ArticleCard from '~/src/components/ArticleCard';
+import { articles } from '~/src/data/articles';
 
 const Bookmarks = () => {
   return (
-    <ScrollView className="p-4">
-      <Text className="text-4xl font-bold">Bookmarks</Text>
+    <ScrollView className="px-4 pt-10 dark:bg-black">
+      <Text className="text-4xl font-bold dark:text-white">Bookmarks</Text>
       <View className="mt-5 flex flex-col gap-5">
-        {[...Array(3)].map((_, index) => (
-          <ArticleCard bookmark={true} key={index} />
-        ))}
+        {articles
+          .filter((article) => article.isBookmarked)
+          .map((article, index) => (
+            <Link
+              href={{ pathname: '/ArticleScreen', params: { article: JSON.stringify(article) } }}
+              key={index}>
+              <ArticleCard bookmark={true} article={article} />
+            </Link>
+          ))}
       </View>
     </ScrollView>
   );
