7c58afa4f77615c23e0806d2841b68bc4d88a2d5
Udara Lakshan <ulakshan061@gmail.com>
2025-05-27 17:18:08 +0530

    Refactor Bookmarks component to fetch articles from store and improve loading state handling

--- frontend/src/app/(tabs)/bookmarks/index.tsx
+++ frontend/src/app/(tabs)/bookmarks/index.tsx
@@ -1,22 +1,47 @@
 import { Link } from 'expo-router';
+import { useEffect, useState } from 'react';
 import { View, Text, ScrollView } from 'react-native';
 import ArticleCard from '~/src/components/ArticleCard';
-import { articles } from '~/src/data/articles';
+import { useStore } from '~/src/store/store';
+import { Article } from '~/src/types/types';
 
 const Bookmarks = () => {
+  const [bookmarkedArticles, setBookmarkedArticles] = useState<Article[]>([]);
+
+  const [loading, setLoading] = useState(true);
+
+  const { articles, getArticles } = useStore();
+
+  useEffect(() => {
+    const fetchArticles = async () => {
+      setLoading(true);
+      await getArticles();
+      setLoading(false);
+    };
+    fetchArticles();
+  }, [getArticles]);
+
+  useEffect(() => {
+    setBookmarkedArticles(articles.filter((article) => article.isBookmarked));
+  }, [articles]);
+
   return (
     <ScrollView className="px-4 pt-10 dark:bg-black">
       <Text className="text-4xl font-bold dark:text-white">Bookmarks</Text>
-      <View className="mt-5 flex flex-col gap-5">
-        {articles
-          .filter((article) => article.isBookmarked)
-          .map((article, index) => (
+      <View className="flex flex-col gap-5 mt-5">
+        {loading ? (
+          <Text>Loading...</Text>
+        ) : bookmarkedArticles.length !== 0 ? (
+          bookmarkedArticles.map((article, index) => (
             <Link
               href={{ pathname: '/ArticleScreen', params: { article: JSON.stringify(article) } }}
               key={index}>
               <ArticleCard bookmark={true} article={article} />
             </Link>
-          ))}
+          ))
+        ) : (
+          <Text className="text-white">No Bookmarked Articles</Text>
+        )}
       </View>
     </ScrollView>
   );
