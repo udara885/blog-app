e8b3aa78ec270892d24af0e72c84ab19379f814b
Udara Lakshan <ulakshan061@gmail.com>
2025-05-28 11:59:27 +0530

    Refactor Search component to improve article fetching and display random proposed articles

--- frontend/src/app/(tabs)/search/index.tsx
+++ frontend/src/app/(tabs)/search/index.tsx
@@ -2,20 +2,33 @@ import { View, Text, TextInput, ScrollView } from 'react-native';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import ArticleCard from '~/src/components/ArticleCard';
 import { Link } from 'expo-router';
-import { useStore } from '~/src/store/store'
-import { useEffect } from 'react'
+import { useStore } from '~/src/store/store';
+import { useEffect, useState } from 'react';
+import { Article } from '~/src/types/types';
 
 const arr = ['iOS 15 beta', 'iPad mini 6', 'iPadOS 15 beta', 'Save battery'];
 
 const Search = () => {
-  const { articles, getArticles } = useStore()
-  
+  const { articles, getArticles } = useStore();
+
+  const [randomProposeArticles, setRandomProposeArticles] = useState<Article[]>([]);
+
   useEffect(() => {
     const fetchAllArticles = async () => {
-      await getArticles()
+      await getArticles();
+    };
+    fetchAllArticles();
+  }, [getArticles]);
+
+  useEffect(() => {
+    if (articles.length > 0) {
+      const getRandomArticles = () => {
+        const shuffled = [...articles].sort(() => 0.5 - Math.random());
+        return shuffled.slice(0, Math.min(4, articles.length));
+      };
+      setRandomProposeArticles(getRandomArticles());
     }
-    fetchAllArticles()
-  },[getArticles])
+  }, [articles]);
 
   return (
     <ScrollView className="px-4 pt-10 dark:bg-black">
@@ -23,7 +36,7 @@ const Search = () => {
       <View className="relative flex flex-row items-center justify-center mt-4">
         <FontAwesome name="search" size={20} color="#8e8e93" className="absolute z-10 left-3" />
         <TextInput
-          placeholder="iOS 15, iPhone 13"
+          placeholder="Search"
           placeholderTextColor="#8e8e93"
           className="flex-1 rounded-xl bg-gray-200 px-10 dark:bg-[#212529] dark:text-white"
         />
@@ -41,7 +54,7 @@ const Search = () => {
       <View className="-mx-4 mt-5 px-4 py-4 dark:bg-[#212529] ">
         <Text className="pb-2 text-xl font-bold dark:text-white">Propose</Text>
         <View className="flex flex-col gap-5 py-10 pt-5 border-t border-gray-200 dark:border-gray-500">
-          {articles.map((article, index) => (
+          {randomProposeArticles.map((article, index) => (
             <Link
               href={{ pathname: '/ArticleScreen', params: { article: JSON.stringify(article) } }}
               key={index}>
