eb24688d8b24ecbbfd99d40bd52906e5d7e5c0cb
Udara Lakshan <ulakshan061@gmail.com>
2025-05-29 13:33:48 +0530

    Refactor Comment interface to improve structure and clarity

--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -6,4 +6,11 @@ export interface Article {
   description: string;
   createdAt?: Date;
   isBookmarked?: boolean;
+  comments?: Comment[];
+}
+
+export interface Comment {
+  name: string;
+  email: string;
+  comment: string;
 }
