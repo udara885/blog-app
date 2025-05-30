117efa0b6aaf77c03fe7169a859f7ebedc969d17
Udara Lakshan <ulakshan061@gmail.com>
2025-05-30 13:21:53 +0530

    Refactor Bookmarks component to simplify loading and no bookmarks views

--- frontend/src/app/(tabs)/bookmarks/index.tsx
+++ frontend/src/app/(tabs)/bookmarks/index.tsx
@@ -1,4 +1,4 @@
-import AntDesign from '@expo/vector-icons/AntDesign'
+import AntDesign from '@expo/vector-icons/AntDesign';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Link } from 'expo-router';
 import { useEffect, useState } from 'react';
@@ -27,29 +27,33 @@ const Bookmarks = () => {
     setBookmarkedArticles(articles.filter((article) => article.isBookmarked));
   }, [articles]);
 
+  if (loading)
+    return (
+      <View className="flex flex-col items-center justify-center h-full gap-3 dark:bg-black">
+        <AntDesign name="loading1" size={50} color="gray" />
+        <Text className="text-xl font-bold text-gray-400">Loading...</Text>
+      </View>
+    );
+
+  if (bookmarkedArticles.length <= 0)
+    return (
+      <View className="flex flex-col items-center justify-center h-full gap-3 dark:bg-black">
+        <FontAwesome name="bookmark" size={50} color="gray" />
+        <Text className="text-xl font-bold text-gray-400">No Bookmarks</Text>
+      </View>
+    );
+
   return (
     <ScrollView className="px-4 pt-10 dark:bg-black">
       <Text className="text-4xl font-bold dark:text-white">Bookmarks</Text>
       <View className="flex flex-col gap-5 mt-5">
-        {loading ? (
-          <View className="flex flex-col items-center justify-center h-full gap-3 mt-20">
-            <AntDesign name="loading1" size={50} color="gray" />
-            <Text className="text-xl font-bold text-gray-400">Loading...</Text>
-          </View>
-        ) : bookmarkedArticles.length !== 0 ? (
-          bookmarkedArticles.map((article, index) => (
-            <Link
-              href={{ pathname: '/ArticleScreen', params: { article: JSON.stringify(article) } }}
-              key={index}>
-              <ArticleCard bookmark={true} article={article} />
-            </Link>
-          ))
-        ) : (
-          <View className="flex flex-col items-center justify-center h-full gap-3 mt-20">
-            <FontAwesome name="bookmark" size={50} color="gray" />
-            <Text className="text-xl font-bold text-gray-400">No Bookmarks</Text>
-          </View>
-        )}
+        {bookmarkedArticles.map((article, index) => (
+          <Link
+            href={{ pathname: '/ArticleScreen', params: { article: JSON.stringify(article) } }}
+            key={index}>
+            <ArticleCard bookmark={true} article={article} />
+          </Link>
+        ))}
       </View>
     </ScrollView>
   );
